87634166d787639045cd6c0c1bb45d412fe59bd4
Piran Tata <piran.tata@gmail.com>
2016-03-13 14:32:57 +1300

    Added ability to detect error conditions on RequestMessage

--- project/DpsPayfit.Client.Test/RequestMessageFixture.cs
+++ project/DpsPayfit.Client.Test/RequestMessageFixture.cs
@@ -6,15 +6,21 @@
         {
             Uri = "http://test.com";
             ValidFlag = 1;
+            ResponseCode = string.Empty;
+            ResponseText = string.Empty;
         }
         public RequestMessage Build() {
             return new RequestMessage {
                 Uri = Uri,
-                ValidFlag = ValidFlag
+                ValidFlag = ValidFlag,
+                ResponseCode = ResponseCode,
+                ResponseText = ResponseText
             };
         }
 
         public string Uri { get; set; }
         public int ValidFlag { get; set; }
+        public string ResponseCode { get; private set; }
+        public string ResponseText { get; private set; }
     }
 }
--- project/DpsPayfit.Client/RequestMessage.cs
+++ project/DpsPayfit.Client/RequestMessage.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace DpsPayfit.Client
 {
@@ -13,10 +8,24 @@ namespace DpsPayfit.Client
         [XmlAttribute(AttributeName = "valid")]
         public int ValidFlag { get; set; }
 
-        [XmlIgnore]
-        public bool IsValid { get { return ValidFlag == 1; } }
-
         [XmlElement(ElementName = "URI")]
         public string Uri { get; set; }
+
+        [XmlElement(ElementName = "Reco")]
+        public string ResponseCode { get; set; }
+
+        [XmlElement(ElementName = "ResponseText")]
+        public string ResponseText { get; set; }
+
+        [XmlIgnore]
+        public bool IsValid
+        {
+            get
+            {
+                return ValidFlag == 1
+                && string.IsNullOrWhiteSpace(ResponseCode)
+                && string.IsNullOrWhiteSpace(ResponseText);
+            }
+        }
     }
 }
